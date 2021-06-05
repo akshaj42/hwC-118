19dde6c457d5cf5422d03f9ed1c713c711d4e13c
akshaj42 <76961627+akshaj42@users.noreply.github.com>
2021-06-05 09:52:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//https://teachablemachine.withgoogle.com/models/H_p3v9S-9/
+
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
